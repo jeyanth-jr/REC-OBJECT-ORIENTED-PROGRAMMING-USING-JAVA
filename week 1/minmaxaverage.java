548b52ae263dcc9e8e3f3017d04e5b813ced3068
Jeyanth <66862827+jeyanth-jr@users.noreply.github.com>
2022-08-16 20:18:25 +0530

    week 1 programs

--- week 1/minmaxaverage.java
+++ week 1/minmaxaverage.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class minmax {
+public class minmaxaverage {
     public static  void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int a,b,min,max,average;
